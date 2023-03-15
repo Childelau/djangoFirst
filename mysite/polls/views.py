7b118aede79009324bf0618dd2cd35fe0247afb0
liuw <1299337429@qq.com>
2023-03-15 15:22:03 +0800

    part4finished

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
